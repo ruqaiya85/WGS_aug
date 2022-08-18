bcab44ba334d294841c7c81488d9a2a9e639c422
ruqaiya85 <rukaiyak890@gmail.com>
2022-08-18 16:00:09 +0530

    math and cal are added

---
+++ src/basic/Calculator.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Calculator {
+	public static void main(String[]args) {
+        System.out.println("Addition");
+        Math.add(27,67);
+        Math.add(65,89,43);
+        System.out.println("Multiplication");
+        Math.multiply(23,8);
+    }
+
+}
---
+++ src/basic/Math.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class Math {
+	public static void add(int a,int b) {
+		System.out.println("Sum is :" + a+b);
+	}
+	public static void add(int a,int b,int c) {
+	System.out.println("Sum is :" +a+b+c);
+	}
+	public static void multiply(int a,int b) {
+		System.out.println("Product is " +a*b);
+	}
+}
